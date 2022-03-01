a3e82103a92384b1a159218da77781f39594030f
SW1pr0g <poryadin_alex@mail.ru>
2022-03-02 01:46:53 +0300

    check a gitignore

--- classes/reversedate.cs
+++ classes/reversedate.cs
@@ -28,21 +28,22 @@ namespace AIS_exchangeOffice.classes
         }
         public string dateReverse(string s)
         {
-            s = s.Replace('.', '-');
-            char[] charArr = s.ToCharArray();
-            char[] charArr1 = new char[charArr.Length];
-            Array.Copy(charArr, charArr1, charArr.Length);
-            charArr[0] = charArr1[6];
-            charArr[1] = charArr1[7];
-            charArr[2] = charArr1[8];
-            charArr[3] = charArr1[9];
-            charArr[4] = '-';
-            charArr[5] = charArr1[0];
-            charArr[6] = charArr1[1];
-            charArr[7] = '-';
-            charArr[8] = charArr1[3];
-            charArr[9] = charArr1[4];
-            return new string(charArr);
+            //date Reversing
+            //s = s.Replace('.', '-');
+            //char[] charArr = s.ToCharArray();
+            //char[] charArr1 = new char[charArr.Length];
+            //Array.Copy(charArr, charArr1, charArr.Length);
+            //charArr[0] = charArr1[6];
+            //charArr[1] = charArr1[7];
+            //charArr[2] = charArr1[8];
+            //charArr[3] = charArr1[9];
+            //charArr[4] = '-';
+            //charArr[5] = charArr1[0];
+            //charArr[6] = charArr1[1];
+            //charArr[7] = '-';
+            //charArr[8] = charArr1[3];
+            //charArr[9] = charArr1[4];
+            return s;
         }
     }
 }
